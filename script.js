d178cb841a7ed185f377fde4343956fc17727230
danya.pac3 <danya.pac3@gmail.com>
2025-02-22 14:44:32 +0200

    Refactor code

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function createElementWithClasses(elementType, ...classes) {
   return element;
 }
 
-function generateCanvas(size = 16) {
+function generateCanvas(size) {
   const canvas = createElementWithClasses('div', 'canvas-container');
   canvas.style.setProperty('--cell-size', `calc(100% / ${size})`);
   for (let i = 0; i < size ** 2; i++) {
@@ -14,9 +14,25 @@ function generateCanvas(size = 16) {
   return canvas;
 }
 
+function initCanvas(size = 16) {
+  const oldCanvas = document.querySelector('.canvas-container');
+  const newCanvas = generateCanvas(size);
+
+  newCanvas.addEventListener('mousedown', (e) => {
+    if (e.button === 0) isMouseDown = true;
+    paintCell(e.target);
+  });
+  newCanvas.addEventListener('mouseover', (e) => {
+    paintCell(e.target);
+  });
+
+  oldCanvas.replaceWith(newCanvas)
+  return newCanvas;
+}
+
 function paintCell(cell) {
   if (isMouseDown && cell.classList.contains('canvas-cell')) {
-    cell.style.backgroundColor = 'black';
+    cell.style.backgroundColor = cellColor;
   }
 }
 
@@ -27,24 +43,18 @@ function toggleGridVisibility() {
   else canvasContainer.style.removeProperty('--cell-border', '');
 }
 
-let canvasContainer = document.querySelector('.canvas-container');
+// Global Variables
+let canvasContainer;
 let toggleGridVisibilityButton = document.querySelector('.toggle-grid-visibility');
 let isMouseDown = false;
+let cellColor = ' #bf5a6c';
 
-canvasContainer.replaceWith(generateCanvas());
-canvasContainer = document.querySelector('.canvas-container');
+// Init canvas
+canvasContainer = initCanvas();
 
+// Event Listeners
 toggleGridVisibilityButton.addEventListener('click', toggleGridVisibility);
-
-document.addEventListener('mousedown', (e) => {
-  if (e.button === 0) isMouseDown = true;
-  paintCell(e.target);
-});
 
 document.addEventListener('mouseup', (e) => {
   isMouseDown = false;
-});
-
-document.addEventListener('mouseover', (e) => {
-  paintCell(e.target);
 });
